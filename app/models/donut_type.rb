f0f03ea7f80292e09d5fa73ceb1ab99248d2fea2
KennyKay <kennykallas@gmail.com>
2023-08-10 16:26:00 -0400

    Migrate schema

--- app/models/donut_type.rb
+++ app/models/donut_type.rb
@@ -1,2 +1,7 @@
 class DonutType < ApplicationRecord
+    attribute :name, default: ""
+    attribute :sold_out_until, default: nil
+    attribute :discontinued, default: false, null: false
+    attribute :price, default: 0
+    validates :name, :discontinued, :price, presence: true
 end
--- db/migrate/20230810202439_create_donut_types.rb
+++ db/migrate/20230810202439_create_donut_types.rb
@@ -1,10 +1,10 @@
 class CreateDonutTypes < ActiveRecord::Migration[7.0]
   def change
     create_table :donut_types do |t|
-      t.string :name
-      t.date :sold_out_until
-      t.boolean :discontinued
-      t.decimal :price
+      t.string :name, null: false
+      t.date :sold_out_until, default: nil
+      t.boolean :discontinued, default: false, null: false
+      t.decimal :price, default: 0
 
       t.timestamps
     end
