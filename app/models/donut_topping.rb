71dcf00a7db7333cf49891db9db592cd17d367f9
KennyKay <kennykallas@gmail.com>
2023-08-10 18:17:00 -0400

    Donut Topping Model

--- app/models/donut_topping.rb
+++ app/models/donut_topping.rb
@@ -1,2 +1,7 @@
 class DonutTopping < ApplicationRecord
+    attribute :name, default: ""
+    attribute :sold_out_until, default: nil
+    attribute :discontinued, default: false, null: false
+    attribute :price, default: 0
+    validates :name, :discontinued, :price, presence: true
 end
