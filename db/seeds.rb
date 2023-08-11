71e9b225bba2055f885db031163dd5fcc1cbe54e
KennyKay <kennykallas@gmail.com>
2023-08-11 09:19:13 -0400

    define toppings and donut types seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+DonutType.destroy_all
+DonutTopping.destroy_all
+DonutType.create!(name: "Raised", price: 1.0, discontinued: false)
+DonutType.create!(name: "Cake", price: 1.5, discontinued: false)
+DonutType.create!(name: "Bars", price: 2.0, discontinued: false)
+DonutType.create!(name: "Twists", price: 3.0, discontinued: false)
+DonutType.create!(name: "Fritters", price: 1.3, discontinued: false)
+
+DonutTopping.create!(name: "Glazed", price: 0.5, discontinued: false)
+DonutTopping.create!(name: "Sugar", price: 0.5, discontinued: false)
+DonutTopping.create!(name: "Powder", price: 0.5, discontinued: false)
+DonutTopping.create!(name: "Sprinkles", price: 0.5, discontinued: false)
+DonutTopping.create!(name: "Fillings", price: 1.0, discontinued: false)
