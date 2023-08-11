3542786c3664f895aec8e7b134ce47666f11f269
KennyKay <kennykallas@gmail.com>
2023-08-11 08:42:47 -0400

    Add cancelled status for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :donut_order_items
-  enum :status, [ :processing, :delivery, :completed ]
+  enum :status, [ :processing, :delivery, :completed, :cancelled ]
 end
