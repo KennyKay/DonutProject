3b2442071f20e08b6f36c796925e098216961a8c
KennyKay <kennykallas@gmail.com>
2023-08-11 08:43:13 -0400

    Add basic orders_controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :donut_toppings, except: [:destroy]
   resources :donut_types, except: [ :destroy]
+  resources :orders, except: [ :destroy]
+  
   get 'program/index'
   root 'program#index'
   post 'program/add_various_donut_types'
