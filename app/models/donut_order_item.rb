6c2597ce04270d981cf0bf4846e27ab25a0d39b4
KennyKay <kennykallas@gmail.com>
2023-08-12 08:33:09 -0400

    Add amount methods on donut order item and order models

--- app/models/donut_order_item.rb
+++ app/models/donut_order_item.rb
@@ -2,4 +2,8 @@ class DonutOrderItem < ApplicationRecord
   belongs_to :donut_type
   has_and_belongs_to_many :donut_toppings
   attribute :quantity, default: 1
+
+  def amount
+    (donut_type.price + donut_toppings.sum(:price)) * quantity
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,8 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :donut_order_items
   enum :status, [ :processing, :delivery, :completed, :cancelled ]
+
+  def amount
+    donut_order_items.sum(:amount)
+  end
 end
